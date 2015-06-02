a871fe9175e7a6549d30e9323cba0fb2e7a60899
SamDecrock <sam.decrock@gmail.com>
2015-06-02 14:42:24 +0200

    added RSSI message, starts with 'R'

--- woopibotRFDuino/woopibotRFDuino.ino
+++ woopibotRFDuino/woopibotRFDuino.ino
@@ -48,3 +48,26 @@ void RFduinoBLE_onReceive(char *data, int len)
   digitalWrite(led, HIGH);
   Serial.print(data);
 }
+
+// returns the dBm signal strength indicated by the receiver
+// received signal strength indication (-0dBm to -127dBm)
+void RFduinoBLE_onRSSI(int rssi) {
+  
+  // convert int to string and string to char array:
+  String rssiString = String(rssi);
+  int rssiStringLen = rssiString.length()+1; 
+  char rssiCharArray[rssiStringLen];
+  rssiString.toCharArray(rssiCharArray, rssiStringLen);
+  
+  
+  // prepare BLE data:
+  char data[rssiStringLen+3];
+  data[0] = 0x02;
+  data[1] = 'R';
+  for(int i=0; i < rssiStringLen-1; i++) {
+    data[i+2] = rssiCharArray[i];
+  }
+  data[rssiStringLen+1] = 0x03;
+  
+  RFduinoBLE.send(data, rssiStringLen+2); 
+}
