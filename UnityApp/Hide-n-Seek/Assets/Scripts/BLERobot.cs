dab5857db4f7d1c1d24ae5f7c299c83cacbb9c45
SamDecrock <sam.decrock@gmail.com>
2015-06-03 13:49:26 +0200

    rewrote NFC2BLEcards as BLERobotManager and BLERobot

---
+++ UnityApp/Hide-n-Seek/Assets/Scripts/BLERobot.cs
@@ -0,0 +1,162 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class BLERobot
+{
+	private BluetoothDeviceScript bluetoothDeviceScript;
+	private string characteristicUUID = "2221";
+
+	public string id;  // in uuid form, eg: 92B314BC-B02E-4926-B8E8-D5B48D69B9A6
+	public string name;
+	public int rssi;
+
+	public bool isConnected = false;
+
+	
+	private List<Byte> receivingCommandbytes = new List<Byte>();
+	private bool receivingCommandStarted = false;
+			
+			
+	public BLERobot (BluetoothDeviceScript bluetoothDeviceScript, string name, string id)
+	{
+		this.bluetoothDeviceScript = bluetoothDeviceScript;
+		this.name = name;
+		this.id = id;
+
+		Debug.Log ("new robot object created");
+	}
+
+	public void Connect()
+	{
+		Debug.Log ("BLE - " + this.id + " : connecting");
+		BluetoothLEHardwareInterface.ConnectToPeripheral(this.id, ConnectedPeripheralAction, DiscoveredServiceAction, DiscoveredCharacteristicAction, DisconnectedPeripheralAction);
+	}
+
+	public void ConnectedPeripheralAction(string identifier)
+	{
+		if (!isConnected) {
+			Debug.Log ("BLE - " + this.id + " : connected");
+			isConnected = true;
+		}
+	}
+
+	public void DisconnectedPeripheralAction(string identifier)
+	{
+		isConnected = false;
+
+		Debug.Log ("BLE - " + this.id + " : disconnected!");
+
+		// doorgeven aan de robotmanager dat we gedisconnect zijn
+		BLERobotManager.Instance.robotDisconnected (this);
+
+		// terug scannen voor nieuwe peripherals:
+		BLERobotManager.Instance.ScanForPeripherals ();
+	}
+	
+	public void DiscoveredServiceAction(string id, string serviceUUID)
+	{
+		Debug.Log ("BLE - " + this.id + " : discoved service uuid '" + serviceUUID + "'");
+	}
+	
+	public void DiscoveredCharacteristicAction(string id, string serviceUUID, string characteristicUUID)
+	{
+		Debug.Log ("BLE - " + this.id + " : discoved characteristic uuid '" + characteristicUUID + "'");
+
+		if(characteristicUUID == this.characteristicUUID)
+		{
+			Debug.Log ("BLE - " + this.id + " : subscribing to notifications");
+			BluetoothLEHardwareInterface.SubscribeCharacteristic (this.id, serviceUUID, characteristicUUID, DidUpdateNotifiationStateForCharacteristicAction, DidUpdateCharacteristicValueAction);
+		
+
+			GameManager.Instance.onRobotConnected(this);
+		}
+	}
+
+	public void DidUpdateNotifiationStateForCharacteristicAction(string characteristicUUID)
+	{
+		Debug.Log ("BLE - " + this.id + " : DidUpdateNotifiationStateForCharacteristicAction()");
+	}
+	
+	public void DidUpdateCharacteristicValueAction(string characteristicUUID, byte[] data)
+	{
+
+		for (int i = 0; i<data.Length; i++) {
+			switch(data[i]) {
+			case 0x02:
+				receivingCommandStarted = true;
+				receivingCommandbytes.Clear();
+				break;
+
+			case 0x03:
+				if(receivingCommandStarted) {
+					parseCommand(receivingCommandbytes.ToArray());
+					receivingCommandStarted = false;
+				}
+				break;
+
+			default:
+				if(receivingCommandStarted){
+					receivingCommandbytes.Add(data[i]);
+				}
+				break;
+			}
+		}
+	}
+
+	public void parseCommand(byte[] command) {
+		string commandString = Encoding.UTF8.GetString(command, 0, command.Length);
+		Debug.Log("BLE - " + this.id + " : incomming command " + commandString);
+
+		string commandWithoutFirstLetter = commandString.Substring (1);
+		string firstLetter = commandString.Substring (0, 1);
+
+
+		switch (firstLetter) {
+		case "S":
+			incommingLineSensor(commandWithoutFirstLetter);
+			break;
+				
+		case "P":
+			incommingPingSensor(commandWithoutFirstLetter);
+			break;
+					
+		case "R":
+			incommingRSSI(commandWithoutFirstLetter);
+			break;
+		}
+	}
+
+	public void incommingLineSensor(string data)
+	{
+		Debug.Log("BLE - " + this.id + " : incomming line sensor data: " + data);
+	}
+	
+	public void incommingPingSensor(string data)
+	{
+		Debug.Log("BLE - " + this.id + " : incomming ping sensor data: " + data);
+	}
+	
+	public void incommingRSSI(string data)
+	{
+		try
+		{
+			this.rssi = Convert.ToInt32(data);
+			
+			float normalizedRssi = ((float)rssi + 255) / 255;
+			
+			Debug.Log("BLE - " + this.id + " : incomming rssi: " + rssi + "| normalized value: " + normalizedRssi);
+
+			GameManager.Instance.onRSSIUpdate(this, rssi);
+		}
+		catch (FormatException e)
+		{
+			Debug.Log("BLE - " + this.id + " : rssi value is no Int32");
+		}
+	}
+
+}
+
+
---
+++ UnityApp/Hide-n-Seek/Assets/Scripts/BLERobotManager.cs
@@ -0,0 +1,72 @@
+/*
+ * Ter info: BluetoothLEHardwareInterface.Log (str); logt naar de iOS output window onafhankelijk van het feit of "Script Debugging" aanstaat of niet
+ * 
+ * */
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BLERobotManager : Singleton<BLERobotManager>
+{
+	private BluetoothDeviceScript bluetoothDeviceScript;
+	string allowedName = "woopibot";
+	string serviceUUID = "2220";
+
+
+	private List<BLERobot> bleRobots = new List<BLERobot>();
+	
+
+	public void StartBLE() // aangeroepen van SetupSceneScript
+	{
+		Debug.Log("BLE: initalizing");
+		bluetoothDeviceScript = BluetoothLEHardwareInterface.Initialize (true, false, InitializedAction, ErrorAction);
+	}
+
+	// Callbacks (Actions/Delegates):
+	public void InitializedAction ()
+	{
+		Debug.Log("BLE: initialized");
+		ScanForPeripherals ();
+	}
+	
+	public void ErrorAction (string error)
+	{
+		// TODO: show error in UI
+		Debug.Log("BLE: initialization error: " + error);
+	}
+
+
+	public void ScanForPeripherals()
+	{
+		Debug.Log("BLE: scanning for peripherals with service uuid '" + serviceUUID + "'");
+		BluetoothLEHardwareInterface.ScanForPeripheralsWithServices (new string[] { serviceUUID }, DiscoveredPeripheralAction);
+	}
+
+	public void DiscoveredPeripheralAction(string identifier, string name)
+	{
+		Debug.Log("BLE - " + identifier + " : discovered peripheral with name '" + name + "'");
+		if(name != allowedName) Debug.Log("BLE - " + identifier + " : not connecting");
+
+		// maak een nieuw robot objectje aan:
+		BLERobot robot = new BLERobot (bluetoothDeviceScript, name, identifier);
+
+		// connect en doe verder met BLE stuff in het objectje:
+		robot.Connect ();
+	}
+
+
+	public void robotDisconnected(BLERobot robot)
+	{
+		if(bleRobots.Contains(robot))
+			bleRobots.Remove(robot);
+
+		robot = null;
+	}
+
+
+
+}
+
+
